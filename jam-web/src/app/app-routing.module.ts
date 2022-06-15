d55f4bd84d8dfee65d72ebbd7b055ffd64041882
Paolo Posso <pvictorsys@gmail.com>
2022-06-15 11:15:47 -0300

    create profile page

--- jam-web/src/app/app-routing.module.ts
+++ jam-web/src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  }
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
+  },
 ];
 
 @NgModule({
