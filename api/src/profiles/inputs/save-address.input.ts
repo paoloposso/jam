1be4efc6b0ff6e75a09d0c1ee462efd7a82d557b
Paolo Posso <pvictorsys@gmail.com>
2022-06-16 12:00:46 -0300

    Refactoring

--- api/src/profiles/inputs/save-address.input.ts
+++ api/src/profiles/inputs/save-address.input.ts
@@ -1,7 +1,7 @@
 import { Field, InputType } from "@nestjs/graphql";
 
 @InputType()
-export class EditAddressInput  {
+export class SaveAddressInput  {
     @Field((type) => String, {nullable: false})
     id: string;
 
--- api/src/profiles/mongo/profile.repository.ts
+++ api/src/profiles/mongo/profile.repository.ts
@@ -3,7 +3,6 @@ import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { LocationEntity } from "../entities/location.entity";
 import { ProfileEntity } from "../entities/profile.entity";
-import { ProfileLocationModel } from "./location.schema";
 import { ProfileDocument } from "./profile.schema";
 
 @Injectable()
