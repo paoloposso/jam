d51eefb9110f26f45769130c557365d4a346fa92
Paolo Posso <pvictorsys@gmail.com>
2022-06-17 09:50:33 -0300

    create address component and add sample fields

--- jam-web/src/app/profile/profile.module.ts
+++ jam-web/src/app/profile/profile.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { ProfilePageRoutingModule } from './profile-routing.module';
 
 import { ProfilePage } from './profile.page';
+import { AddressComponent } from './address/address.component';
 
 @NgModule({
   imports: [
@@ -15,6 +16,6 @@ import { ProfilePage } from './profile.page';
     IonicModule,
     ProfilePageRoutingModule
   ],
-  declarations: [ProfilePage]
+  declarations: [ProfilePage, AddressComponent]
 })
 export class ProfilePageModule {}
