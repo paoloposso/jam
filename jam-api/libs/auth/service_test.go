84c0057e672749cb466a67014804d9e16918ce16
Paolo Posso <pvictorsys@gmail.com>
2023-03-22 14:36:08 -0300

    fix tests location

--- jam-api/libs/auth/service_test.go
+++ jam-api/libs/auth/service_test.go
@@ -27,6 +27,7 @@ func TestLoginUnauthorized(t *testing.T) {
 	if err != nil {
 		switch err.(type) {
 		case *customerrors.UnauthorizedError:
+			t.Fatal("aaaaa")
 			return
 		default:
 			t.Fatal("should return unauthorized error")
